718e3306a30870c17afbec1e2ef4139b90298af5
Jan <gruttefien@gmail.com>
2025-02-06 23:08:45 +0100

    changes

--- visualize.py
+++ visualize.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime, timedelta
 from sql_connector import SqlConnector
-import pytz
 
 def round_down_to_x_minutes(dt, x):
     """
@@ -57,14 +56,7 @@ def visualize_data(range_="last_2_hours", offset=0):
     for entry in data:
         dt_str = entry[1]             # "YYYY-mm-dd HH:MM:SS"
         data_value = entry[2]
-        
         dt = datetime.strptime(dt_str, "%Y-%m-%d %H:%M:%S")
-        
-        # utc to germany timezone
-        germany_tz = pytz.timezone("Europe/Berlin")
-        dt = pytz.utc.localize(dt).astimezone(germany_tz)
-        
-        
         dt_bin = bin_func(dt)         # Round down to the bin
 
         grouped.setdefault(dt_bin, []).append(data_value)
@@ -88,11 +80,6 @@ def visualize_data(range_="last_2_hours", offset=0):
     for entry in verified_data:
         dt_str = entry[1]
         dt = datetime.strptime(dt_str, "%Y-%m-%d %H:%M:%S")
-        
-        # utc to germany timezone
-        germany_tz = pytz.timezone("Europe/Berlin")
-        dt = pytz.utc.localize(dt).astimezone(germany_tz)
-        
         dt_bin = bin_func(dt)
         verified_binned.append(dt_bin)
 
