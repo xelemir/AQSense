b5597d92e1c903ce308ee9f8c70fee53ff247d4f
Jan <gruttefien@gmail.com>
2025-02-07 17:26:48 +0100

    changes

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
             avg_last = sql.get_avg_last_particles(10, 4)
             if avg_last is not None:
                 avg_pm25 = avg_last[0]
-                if pm25 - avg_pm25 > 3 and sql.get_last_marker_within(3) is None and sql.get_last_particle(5) is not None:
+                if pm25 - avg_pm25 > 3.4 and sql.get_last_marker_within(3) is None and sql.get_last_particle(5) is not None:
 
                     # Pollution spike detected, log and send notification if possible
                     if endpoint is not None:
